6852f6bff17af86ba83b967fe59abdfaa6634713
SundayMba <sundinoh@gmail.com>
2024-09-21 03:37:20 +0100

    fix: added data_product

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import "./RelatedProducts.css"
-import data_product from "../../Assets/data"
-import Item from '../Item/Item'
+import React, { useContext } from "react";
+import "./RelatedProducts.css";
+// import data_product from "../../Assets/data"
+import Item from "../Item/Item";
+import { ShopContext } from "../../Context/ShopContext";
 
 const RelatedProducts = () => {
+  const { data_product } = useContext(ShopContext);
   return (
-    <div className='relatedproducts'>
-        <h1>Related Products</h1>
-        <hr />
-        <div className="relatedproducts-item">
-            {data_product.map((item, index) => {
-                return <Item key={index} id={item.id}
-                name={item.name} image={item.image}
-                old_price={item.old_price} new_price={item.new_price}
-                /> 
-            })}
-        </div>
+    <div className="relatedproducts">
+      <h1>Related Products</h1>
+      <hr />
+      <div className="relatedproducts-item">
+        {data_product.map((item, index) => {
+          return (
+            <Item
+              key={index}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              old_price={item.old_price}
+              new_price={item.new_price}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RelatedProducts
+export default RelatedProducts;
